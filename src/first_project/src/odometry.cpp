2903256ad923ba5f360ae3325c78d108f67aea65
Romillon <romain.caprais@mail.polimi.it>
2023-04-29 15:26:15 +0200

    new computation for x y and theta and switched dTi

--- src/first_project/src/odometry.cpp
+++ src/first_project/src/odometry.cpp
@@ -9,6 +9,7 @@
 
 #include <sstream>
 #include <string>
+#include <cmath>
 
 // Global position values
 // This way our service_cb is able to reset them
@@ -27,36 +28,56 @@ static float cstth = 0;
 float speed = 0;
 float steering_angle = 0;
 
-float last_time = 0;
-float new_time = 0;
-float tdelta = 0;
+//float last_time = 0;
+//float new_time = 0;
+//float tdelta = 0;
+
+ros::Time new_time;
+ros::Time last_time;
+ros::Duration tdelta_ROS;
+std::stringstream tdelta;
+
+
 
 void bag_cb(const geometry_msgs::Quaternion::ConstPtr& msg){
 			//ROS_INFO("I heard [%f, %f, %f, %f]", msg->x, msg->y, msg->z, msg->w);
 	// TODO: Get the first time before starting to calculate
-	
-//	last_time = new_time;
-//	new_time = ros::Time::now().toSec();
-//	tdelta = new_time - last_time;
-	
-//	if (last_time == 0){
-//		tdelta = 0;
-//	}
-	speed = msg->x;
-	steering_angle += msg->y;
 
-
-	ROS_INFO("lasttime : [%f]", last_time);
-	ROS_INFO("newtime : [%f]", new_time);
+	double LTime=last_time.toSec(); //
+	ROS_INFO("lasttime : [%f]", LTime);      				//at this point we still have Lasttime =0 for the first loop
+															//after its all good
+	
+			
+	
+	new_time = ros::Time::now(); 
+	tdelta_ROS = new_time -last_time;      
+	last_time=new_time;
+	
+	
+	if (LTime>1)
+	{
+	
+	double tdelta=tdelta_ROS.toSec();		//
+	double NTime=new_time.toSec();         //
+	ROS_INFO("newtime : [%f]", NTime);		//						//the lines below are for the computation of x,y and theta ...
 	ROS_INFO("TDELTA : [%f]", tdelta);
+
+
+	speed = msg->x;
+	steering_angle = msg->y;
+
+		
+	//cstth = static_cast<int>(cstth + steering_angle*tdelta)   %		360;
+	cstth = cstth + steering_angle*tdelta;
+	cstx = cstx + speed *tdelta* sin(cstth*180/M_PI);   //I try to multiply by tdelta in the main loop     *tdelta 
+	csty = csty + speed * tdelta * cos(cstth*180/M_PI);
 	
-	
-	
-	cstx = cstx + speed *  sin(steering_angle);   //I try to multiply by tdelta in the main loop     *tdelta 
-	csty = csty + speed * tdelta * cos(steering_angle);
-	cstth = steering_angle;
 	return;
+	
+	}
 }
+
+
 
 bool service_cb(first_project::reset_odom::Request &req, first_project::reset_odom::Response &res){
 	ROS_INFO("/reset_odom service was called: Setting all odometry values to zero!");
@@ -106,10 +127,13 @@ int main(int argc, char **argv){
 
 	//We iniate our times Values and makeTdelta=0
 
-		ros::Time timestamp= ros::Time::now();;
-		ros::Time previous_timestamp= ros::Time::now();;
-		ros::Duration tdelta_ROS = timestamp-previous_timestamp;
-		std::stringstream tdelta;                                 //still a str we may have to switch for an int
+	//	ros::Time timestamp= ros::Time::now();;
+	//	ros::Time previous_timestamp= ros::Time::now();;
+	//	ros::Duration tdelta_ROS = timestamp-previous_timestamp;
+	//	std::stringstream tdelta;                                 //still a str we may have to switch for an int
+
+
+
 
 	while (ros::ok()){
 		// define messages that we will send
@@ -120,7 +144,7 @@ int main(int argc, char **argv){
 
 		
 
-		//ROS_INFO("%f", ros::Time::now().toSec());
+/*ROS_INFO("%f", ros::Time::now().toSec());
 
 
 
@@ -130,18 +154,18 @@ int main(int argc, char **argv){
 		ROS_INFO("Duration in seconds: %lf", tdelta_ROS.toSec());
 		
 		
-		//cst_msg.x = cst_msg.x + speed * tdelta * sin(steering_angle);
+		cst_msg.x = cst_msg.x + speed * tdelta * sin(steering_angle);    */
 
 
 
-		cst_msg.x = cstx*tdelta_ROS.toSec();
+		cst_msg.x = cstx;
 		cst_msg.y = csty;                           //so here i use csty that comes from    bag_cb   :     
  		cst_msg.th = cstth;										//Question is how can i update cst_msg.y with the value i will cupute here
-		cst_msg.timestamp =   std::to_string(ros::Time::now().toSec())    ;
+		cst_msg.timestamp =   std::to_string(ros::Time::now().toSec()) ;
 		
 
 		msg.pose.pose.position.x = cstx;
-		msg.pose.pose.position.y = posy;
+		msg.pose.pose.position.y = csty;
 		msg.pose.pose.orientation.x = orientx;
 		msg.pose.pose.orientation.y = orienty;
 		msg.pose.pose.orientation.z = orientz;
